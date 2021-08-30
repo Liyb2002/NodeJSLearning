493ab968213d6f2a1907a3c29da16bc13ab85e7e
Liyb2002 <yuanboli@126.com>
2021-08-31 00:01:22 +0800

    +inspection

---
+++ web3/inspection.js
@@ -0,0 +1,20 @@
+const Web3 = require('web3');
+const rpcURL = "https://mainnet.infura.io/v3/686d75be8bf046229804af38cfab9505";
+const web3 = new Web3(rpcURL);
+
+web3.eth.getBlockNumber().then(console.log)
+
+// // get latest block
+//web3.eth.getBlock('latest').then(console.log)
+
+/*
+// get latest 10 blocks
+web3.eth.getBlockNumber().then((latest) => {
+  for (let i = 0; i < 10; i++) {
+    web3.eth.getBlock(latest - i).then(console.log)
+  }
+})*/
+
+// get transaction from specific block
+const hash = '0x66b3fd79a49dafe44507763e9b6739aa0810de2c15590ac22b5e2f0a3f502073'
+web3.eth.getTransactionFromBlock(hash, 2).then(console.log)
