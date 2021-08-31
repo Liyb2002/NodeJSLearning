c532d94a26f5d0521b73c1c8687b3ee44247f89f
Liyb2002 <yuanboli@126.com>
2021-08-31 15:48:28 +0800

    +read from txt

--- web3/sendMessege.js
+++ web3/sendMessege.js
@@ -1,12 +1,12 @@
 var Tx     = require('ethereumjs-tx').Transaction
 const Web3 = require('web3')
+const fs = require('fs');
 const web3 = new Web3("https://rinkeby.infura.io/v3/686d75be8bf046229804af38cfab9505")
 
 const account1 = '0xA006692006545b5EC7Fa7455A3dF5d13BfB07Fb8' // Your account address 1
 const account2 = '0xD4394cb92A9F346468c9F9ee0d1EA3cd4764085B' // Your account address 2
 
-const privateKey1 = Buffer.from('b1d21835b13bfab48f27514beea9c673c75baa55d7e8115e2f7dd575dc188ec9', 'hex')
-const privateKey2 = Buffer.from('ecd88a69abe3d273f010ecb0eb960f53a28bfcd903bfb98d270dfa090f913b34', 'hex')
+const privateKey1 = Buffer.from(fs.readFileSync('./privateKey.txt', 'utf-8'), 'hex')
 
 web3.eth.getTransactionCount(account1, (err, txCount) => {
     // Build the transaction
