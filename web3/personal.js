08507b62267aac74efd14746fe6c6a2323658736
Liyb2002 <yuanboli@126.com>
2021-09-02 19:05:09 +0800

    Update personal.js

--- web3/personal.js
+++ web3/personal.js
@@ -1,8 +1,15 @@
 const Web3 = require('web3');
-const rpcURL = "https://mainnet.infura.io/v3/686d75be8bf046229804af38cfab9505";
+const rpcURL = "https://rinkeby.infura.io/v3/686d75be8bf046229804af38cfab9505";
 const web3 = new Web3(rpcURL);
+const fs = require('fs');
+var Tx     = require('ethereumjs-tx').Transaction
+
 
 var Personal = require('web3-eth-personal')
 var personal = new Personal(rpcURL);
+const password = '12345'
+web3.eth.personal.newAccount(password, function (){
+    console.log("created account")
+})
 
-console.log(web3.shh.currentProvider)
+web3.eth.personal.getAccounts()
