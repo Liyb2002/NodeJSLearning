14d43e02659fd3dd2882b893235a8d1e32fea1dd
Liyb2002 <yuanboli@126.com>
2021-09-02 11:06:58 +0800

    function

---
+++ ch4/function.js
@@ -0,0 +1,21 @@
+var http = require("http");
+
+function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+  
+  http.createServer(onRequest).listen(8888);
+
+  //Same as the following 
+
+  /*
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
+
+
+*/
