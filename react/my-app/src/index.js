e20937057aa07aba4d8329eee13c69610e7d61a3
Liyb2002 <yuanboli@126.com>
2021-09-22 21:26:30 -0400

    img

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ImageButton from 'react-image-button';
+
 
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
+ 
     this.state = {unlocked: true};
 
     // This binding is necessary to make `this` work in the callback
@@ -13,17 +16,32 @@ class Toggle extends React.Component {
   handleClick() {
     if(this.state.unlocked){
       alert("unlocked!")
-    }
+    
     this.setState(prevState => ({
-      unlocked: !prevState.unlocked
+      unlocked: false
     }));
+
+    const interval = setInterval(() => {
+      this.setState(prevState => ({
+        unlocked: true
+      }));
+    }, 5000);
+  
+
+  }
+
+
   }
 
   render() {
     return (
+      <ImageButton img='./b1.png' >
+
       <button onClick={this.handleClick}>
         {this.state.unlocked ? 'ON' : 'Locked'}
       </button>
+      </ImageButton>
+
     );
   }
 }
