25309a3a588fc705c13c261aefb75c4bfba4162a
Liyb2002 <yuanboli@126.com>
2021-09-27 12:05:27 -0400

    +url

--- Django/hw2/src/views.py
+++ Django/hw2/src/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hello hw2")
+def index(request, name, age):
+    #name = request.GET.get('name', '')
+    #age = request.GET.get('age', 0)
+    return HttpResponse('hello i am {0}, age is {1}'.format(name, age))
