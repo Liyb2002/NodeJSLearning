a82525ffdb202961b39b54208a94fb6b06e9a7ed
Liyb2002 <yuanboli@126.com>
2021-10-04 00:43:15 -0400

    4-3F

--- Django/hw2/src/models.py
+++ Django/hw2/src/models.py
@@ -14,3 +14,14 @@ class User(models.Model):
     info = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now = True)
+
+class Userprofile(models.Model):
+    id = models.IntegerField(primary_key = True)
+    user = models.OneToOneField(User, blank=True, null=True, on_delete = models.SET_NULL)
+    birthday = models.CharField(max_length=100, blank=True, default='')
+
+class Group(models.Model):
+    id = models.IntegerField(primary_key = True)
+    user = models.ManyToManyField(User, related_name='group')
+    name = models.CharField(max_length=20)
+    create_time = models.IntegerField()
