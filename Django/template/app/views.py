028f83bebf814ede4a4e2160b0bb7da35e772928
Liyb2002 <yuanboli@126.com>
2021-10-04 19:30:11 -0400

    3-1

--- Django/template/app/views.py
+++ Django/template/app/views.py
@@ -1,3 +1,13 @@
+#coding: utf-8
+
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class Index(View):
+    TEMPLATE = 'index.html'
+
+    def get(self, request, name):
+        return render(request, self.TEMPLATE, 
+        {'name':name})
