e22cf303d017b0303c17c7ecc2d2da4301de3e85
Liyb2002 <yuanboli@126.com>
2021-10-03 23:28:51 -0400

    multi_data

--- Django/hw2/src/models.py
+++ Django/hw2/src/models.py
@@ -5,3 +5,12 @@ from django.db import models
 class Test(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=50, blank=False)
+    age = models.SmallIntegerField(default=0)
+    phone = models.SmallIntegerField(db_index = True, blank=True, default=0)
+    info = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now = True)
