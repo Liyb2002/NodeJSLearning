876713c0ecf8129169da6dce2a40a132f2165576
Liyb2002 <yuanboli@126.com>
2021-10-08 12:19:50 -0400

    f_oct7

--- Django/react1/app/urls.py
+++ Django/react1/app/urls.py
@@ -4,4 +4,4 @@ from .views import showAllPage,add
 urlpatterns = [
     path('showall', showAllPage.as_view(), name='showAll'),
     path('add/<str:name>/<int:price>', add.as_view(), name='showAll')
-]
+] 
