cf272fc93e1006c260dc1aeb26c7fb7f967cfedd
Liyb2002 <yl4092@columbia.edu>
2022-08-23 15:10:28 -0400

    animate

--- webgl/2D/webgl-demo.js
+++ webgl/2D/webgl-demo.js
@@ -139,7 +139,14 @@ function drawScene(gl, programInfo) {
 
   mat4.translate(modelViewMatrix,     // destination matrix
                  modelViewMatrix,     // matrix to translate
-                 [-0.0, 0.0, -6.0]);  // amount to translate
+                 [-0.0, 0.0, -10.0]);  // amount to translate
+
+                 let squareRotation = 1.0;
+
+ mat4.rotate(modelViewMatrix,  // destination matrix
+                 modelViewMatrix,  // matrix to rotate
+                 squareRotation,   // amount to rotate in radians
+                 [0, 0, 1]);    
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
